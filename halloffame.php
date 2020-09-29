b9f8ea4f19ff9facd0b0af77ed779a36903d0586
Elmi Farah <elmifarah2010@hotmail.com>
2020-09-29 16:04:17 +0100

    new

--- halloffame.php
+++ halloffame.php
@@ -5,12 +5,6 @@ $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 $query = $db->prepare('SELECT `name`, `age`,`nationality`,`goal`,`image` FROM `Players`;');
 $query->execute();
 $fame = $query->fetchAll();
-echo '<pre>';
-
-echo '</pre>';
-
-
-
 
 
 
@@ -23,39 +17,45 @@ echo '</pre>';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="halloffame.css">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100&display=swap" rel="stylesheet">
 
     <title>halloffame</title>
 </head>
 <body>
-<div class="navTop">
-    <div class="<img src="halloffame.png"></div>
-    <div class="title">
+    <div class="navTop">
+        <div class="<img src="halloffame.png"></div>
+        <div class="title">
         <h1> FOOTBALLERS HALL OF FAME </h1>
     </div>
-</div>
+    </div>
 
-<header>
-<class="logo"> <img src="football.jpg" alt= "hall of fame logo"/>
-</header>
+    <header>
+        <class="logo"> <img src="new.png" alt= "hall of fame logo"/>
+    </header>
 
-<h3> H.O.F Nominees </h3>
+    <h3> H.O.F Nominees </h3>
 
-<div id="container"
-<div class="Players">
+    <div id="container">
 
-    <?php foreach ($fame as $player) {
+    <div class="Players">
+
+        <?php foreach ($fame as $player) {
         echo "<div class='image'>";
         echo "<img src='" . $player['image'] . "'/>";
         echo "</div>";
         echo "<div class='player'>";
-      echo "<div> {$player['name']} <br> </div>";
-      echo "<div> {$player['age']} <br> </div>";
-      echo "<div> {$player['nationality']} <br> </div>";
-      echo "<div> {$player['goal']} <br> </div>";
+        echo "<div> Name: {$player['name']} <br> </div>";
+        echo "<div> Age: {$player['age']} <br> </div>";
+        echo "<div> Nationality: {$player['nationality']} <br> </div>";
+        echo "<div> Career Goals: {$player['goal']} <br> </div>";
         echo "</div>";
-    }?>
+    }
 
-</div>
+
+
+    ?>
+    </div>
+
 </body>
 <div class="add-player-btn-container">
     <div class="add-player-btn-box-placement">
