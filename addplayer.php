8d3a1a6bee90bb3790de0deeef64d1c81700e9ff
Elmi Farah <elmifarah2010@hotmail.com>
2020-10-01 09:32:34 +0100

    code review

--- addplayer.php
+++ addplayer.php
@@ -5,16 +5,6 @@ $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 $query = $db->prepare('SELECT `name`, `age`,`nationality`,`goal`,`image` FROM `Players`;');
 $query->execute();
 $fame = $query->fetchAll();
-
-
-
-
-
-
-
-
-
-
 ?>
 
 
@@ -22,41 +12,55 @@ $fame = $query->fetchAll();
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="addplayer.css">
+</head>
+<body>
+<div class="navTop">
+    <div class="pictures"> </div>
+</div>
+<h1>            </h1>
+<div class="title"></div>
+<h3>Which Player Deserves to enter the Hall Of Fame? :</h3>
+<div class="header">
+    <div class="page-container">
+        <div class="layout">
+            <div class="container">
+                <form method="post">
+                    <fieldset><input type="text" name="name" placeholder="Name" required=""> <br> </fieldset>
+                    <fieldset><input type="number" name="age" placeholder="Age" required=""> <br> </fieldset>
+                    <fieldset><input type="text" name="nationality" placeholder="Nationality" required=""> <br></fieldset>
+                    <fieldset><input type="number" name="goal" placeholder="Goal" required=""> <br> </fieldset>
+                    <fieldset><input type="text" name="image" placeholder="image" required=""> <br> </fieldset>
+                    <input class="submit-btn" type="submit" name="submit"> <br>
+                </form>
+                <?php
 
- </head>
-  <body>
+                $post = $db->prepare('INSERT INTO `Players` (`name`, `age`,`nationality`,`goal`, `image`) VALUES (:player,:age,:nationality,:goal,:image);');
+                $post->bindParam(':player',$_POST["name"]);
+                $post->bindParam(':age',$_POST["age"]);
+                $post->bindParam(':nationality',$_POST["nationality"]);
+                $post->bindParam(':goal',$_POST["goal"]);
+                $post->bindParam(':image',$_POST["image"]);
+                $post->execute();
 
-  <div class="navTop">
-      <div class="<img src="halloffame.png"></div>
-  <div class="title">
-  </div>
-  </div>
+                ?>
+            </div>
 
-  <h3>Add A Professional Player to the collection:</h3>
-    <div class="header">
-        <div class="page-container">
-            <div class="layout">
-                <div class="container">
-
-                    <form method="post">
-                        <fieldset><input type="text" name="name" placeholder="Name" required=""> <br> </fieldset>
-                        <fieldset><input type="number" name="age" placeholder="Age" required=""> <br> </fieldset>
-                        <fieldset><input type="text" name="nationality" placeholder="Nationality" required=""> <br></fieldset>
-                        <fieldset><input type="text" name="Career-goal" placeholder="Goal" required=""> <br> </fieldset
-                        <fieldset><input type="text" name="images" placeholder="image" required=""> <br> </fieldset
-                        <fieldset><input class="submit-btn" type="submit" name="submit"> <br> </fieldset>
-                    </form>
-                    <div class="btn">
-                    <div class="form-Hall-of-fame-btn">
-                        <a href="halloffame.php" class="back-to-hall-of-fame">Hall Of Fame</a>
-                    </div>
-
-
+            <div class="btn">
+                <div class="form-Hall-of-fame-btn">
+                    <a href="halloffame.php" class="back-to-hall-of-fame">Hall Of Fame</a>
                 </div>
             </div>
         </div>
     </div>
+    <div class="imagess">
+        <div class="ballon">
+        </div>
+        <div class="manu">
+        </div>
+    </div>
+</div>
+</body>
+</html>
--- halloffame.php
+++ halloffame.php
@@ -1,23 +1,19 @@
 <?php
-
 $db = new PDO('mysql:host=db; dbname=halloffame', 'root', 'password');
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 $query = $db->prepare('SELECT `name`, `age`,`nationality`,`goal`,`image` FROM `Players`;');
 $query->execute();
 $fame = $query->fetchAll();
-
-
 ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="halloffame.css">
-    <link rel="stylesheet" type="text/css" href="normalize.css">
+
+<!--    <link rel="stylesheet" type="text/css" href="normalize.css">-->
     <link href="https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@0,600;1,700&family=Raleway:wght@100&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="halloffame.css">
     <title>halloffame</title>
 </head>
 <body>
@@ -31,23 +27,22 @@ $fame = $query->fetchAll();
     <class="logo"> <img src="new.png" alt= "hall of fame logo"/>
 </header>
 <h3> H.O.F Nominees </h3>
-<body id="container">
+<div id="container">
     <div class="players">
         <?php foreach ($fame as $player) {
             echo "<div class='image'>";
             echo "<img src='" . $player['image'] . "'/>";
             echo "</div>";
             echo "<div class='player'>";
-            echo "<div> Name: {$player['name']} <br> </div>";
-            echo "<div> Age: {$player['age']} <br> </div>";
-            echo "<div> Nationality: {$player['nationality']} <br> </div>";
-            echo "<div> Career Goals: {$player['goal']} <br> </div>";
+            echo "<div> Name: {$player['name']}  </div>";
+            echo "<div> Age: {$player['age']} </div>";
+            echo "<div> Nationality: {$player['nationality']}  </div>";
+            echo "<div> Career Goals: {$player['goal']}  </div>";
             echo "<a href='https://www.theguardian.com/football/ng-interactive/2017/dec/19/the-100-best-footballers-in-the-world-2017-interactive'>Get a Better insight here....</a>";
             echo "</div>";
         }
-
         ?>
-
+    </div>
     </div>
 <div class="links">
 <div class="add-player-btn-container">
