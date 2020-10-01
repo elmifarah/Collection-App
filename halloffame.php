887e8379480788462adae94c98142b58d2d21d3b
Elmi Farah <elmifarah2010@hotmail.com>
2020-10-01 13:38:38 +0100

    final code review

--- halloffame.php
+++ halloffame.php
@@ -10,8 +10,7 @@ $fame = $query->fetchAll();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-<!--    <link rel="stylesheet" type="text/css" href="normalize.css">-->
+    <link rel="stylesheet" type="text/css" href="normalize.css">
     <link href="https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@0,600;1,700&family=Raleway:wght@100&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="halloffame.css">
     <title>halloffame</title>
